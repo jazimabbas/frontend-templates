8e21bd6bc0c3d8766937a9e5d3442125c7ad2d57
Jazim Abbas <jazimabbas87@gmail.com>
2024-05-06 14:22:18 +0500

    Contact-us page: Added Content of Hero Section

--- src/app/contact-us/ui/Hero/index.tsx
+++ src/app/contact-us/ui/Hero/index.tsx
@@ -1,7 +1,46 @@
+import { RiBuildingLine, RiMailLine, RiPhoneLine } from "@remixicon/react";
+import {
+  ContentWrapper,
+  Description,
+  FormWrapper,
+  Heading,
+  IconWrapper,
+  Item,
+  Items,
+  Wrapper,
+} from "./Client";
+
 export function Hero() {
   return (
-    <>
-      <p>Hero Section</p>
-    </>
+    <Wrapper>
+      <ContentWrapper>
+        <Heading>Talk to our team</Heading>
+        <Description>
+          {"We're"} committed to delivering the support you require to make your experience as
+          smooth as possible.
+        </Description>
+        <Items>
+          <Item>
+            <IconWrapper>
+              <RiBuildingLine />
+            </IconWrapper>
+            <span>123 Maple Street, Springfield, IL, USA</span>
+          </Item>
+          <Item>
+            <IconWrapper>
+              <RiPhoneLine />
+            </IconWrapper>
+            <span>+1 (650) 555-0198</span>
+          </Item>
+          <Item>
+            <IconWrapper>
+              <RiMailLine />
+            </IconWrapper>
+            <span>hello@abstractly.com</span>
+          </Item>
+        </Items>
+      </ContentWrapper>
+      <FormWrapper />
+    </Wrapper>
   );
 }
