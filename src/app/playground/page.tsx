8daeca88fb989b336547d622fa0b75eae091718c
Jazim Abbas <jazimabbas87@gmail.com>
2024-05-15 19:39:52 +0500

    Design System: Added Badge Component

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
-import { RiStarLine } from "@remixicon/react";
-import { Button } from "@/components/Button";
+import { Badge } from "@/components/Badge";
 
 export const metadata: Metadata = {
   title: "Playground",
@@ -10,14 +9,7 @@ export default function ButtonPage() {
   return (
     <div style={{ margin: "50px" }}>
       <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-        <Button size="lg" leftIcon={RiStarLine}>
-          Button CTA
-        </Button>
-        <Button variant="secondary" size="2xl" rightIcon={RiStarLine}>
-          Button CTA
-        </Button>
-        <Button variant="destructive">Button CTA</Button>
-        <Button iconOnly icon={RiStarLine} />
+        <Badge size="lg" color="success">Label</Badge>
       </div>
     </div>
   );
