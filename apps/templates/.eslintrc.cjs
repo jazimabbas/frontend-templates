57506e16c6a1540cf856f6f800ea8cf6ab7ce42a
Jazim Abbas <jazimabbas87@gmail.com>
2024-05-29 17:15:05 +0500

    Templates: removed local eslint configs and used eslint-config package configs

--- apps/templates/.eslintrc.cjs
+++ apps/templates/.eslintrc.cjs
@@ -1,10 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "next/core-web-vitals", "prettier"],
-  globals: {
-    React: true,
-    JSX: true,
+  root: true,
+  extends: ["@repo/eslint-config/next.js"],
+  rules: {
+    "no-unused-vars": "error",
   },
 };
